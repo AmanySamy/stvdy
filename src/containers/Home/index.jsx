093acaf2e754f684cd2db44a1f3241d9dbf6f798
AmanySamy <Amanysamy628@yahoo.com>
2022-03-02 14:37:38 +0200

    add section 4

--- src/containers/Home/index.jsx
+++ src/containers/Home/index.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import Header from '../../components/Header'
+import Section2 from '../../components/Section2'
+import Section4 from '../../components/section4'
 
 export default function Home() {
     return (
-        <>
-            
+        <>          
             <Header />
+            <Section2 />
+            <Section4 />
         </>
     )
 }
